97051c54cff25ebe9370402847d15926ce136d42
Denis <dan.blinov@yandex.ru>
2020-05-05 21:54:56 +0700

    Updates

---
+++ precache-manifest.b915b182d8dde67955b8d70f631bcd99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90987db37303c67dada965f34e256f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8209a65f303fe8c9c9f5",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "ccf7c574c0c09a1964e4",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "8209a65f303fe8c9c9f5",
+    "url": "./static/js/2.4092c579.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.4092c579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf7c574c0c09a1964e4",
+    "url": "./static/js/main.2725da78.chunk.js"
+  },
+  {
+    "revision": "201d8f674dca02027012",
+    "url": "./static/js/runtime-main.9567efec.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
